acb78498d0945017441d5f9aff7349d3511332fd
Andre Otte <andre.b.otte1@gmail.com>
2019-05-09 16:49:05 -0400

    regex...

--- SVM/PaymentView.cs
+++ SVM/PaymentView.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
 
 namespace SVM
 {
     class PaymentView
     {
 
+
         public void PayCheck()
         {
-            //if(usertotal == FinalTotal)
-            //{
-            //    ReceiptView.
-            //}
-            ReceiptView.PrintReciept(VendingMachine.PurchasedItems);
+            string pattern = "^\d\d\d$";
+            Regex rgx = new Regex();
+            Console.WriteLine("Please enter a check number: ");
+            string userInput = Console.ReadLine().Trim();
+            Int32.TryParse(userInput, out int checkNumber);
+            
+            if(checkNumber > 0)               
+            {
+                ReceiptView.PrintReciept(VendingMachine.PurchasedItems);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid chack number as a 3 digit integer. Please try again.");
+            }
+
         }
 
         public void PayCredit()
