ab932f9f54d80b08680aadde4862c9979f493829
Andre Otte <andre.b.otte1@gmail.com>
2019-05-13 10:32:34 -0400

    fixed a bug in PayCash(), formatting, and comments.

--- SVM/ReceiptView.cs
+++ SVM/ReceiptView.cs
@@ -8,8 +8,10 @@ namespace SVM
 {
     class ReceiptView
     {
+        // The final view after user had paid.
         public static void PrintReciept(List<Power> powers)
         {
+            Console.Clear();
             Console.WriteLine("Receipt for transaction purchases:\n");
             Console.WriteLine($"{AddSpaces("Item")}Price");
             for (int i = 0; i < powers.Count; i++)
@@ -23,6 +25,7 @@ namespace SVM
             Console.WriteLine($"\n{AddSpaces("Total: ")}${MenuView.FinalTotal}");
         }
 
+        // Formatting for receipt and cart views
         public static string AddSpaces(string input)
         {
             int spaces = 30 - input.Length;
@@ -32,6 +35,8 @@ namespace SVM
             }
             return input;
         }
+
+        // View when user selects checkout.
         public static void PrintCart(List<Power> BoughtPowers, List<Power> MasterList)
         {
             Console.WriteLine("Here is your cart:\n");
@@ -54,6 +59,9 @@ namespace SVM
                 "\n3) credit");
             int choice = 0;
             Validator.IsInRangeIndex(Console.ReadLine(), 1, 3, out choice);
+            Console.Clear();
+
+            // Ask the user how they want to pay.
             switch(choice)
             {
                 case 0:
@@ -70,8 +78,33 @@ namespace SVM
                     break;
             }
 
+            // After user has viewed cart, paid, and viewed receipt, ask if they want to keep shopping and initiate new session
+            Console.Write("Keep shopping? (Y/N): ");
 
+            bool again = true;
 
+            while (again)
+            {
+                string newShopping = Console.ReadLine().Trim().ToLower();
+
+                if (newShopping == "y" || newShopping == "yes")
+                {
+                    VendingMachine vendingMachine = new VendingMachine(MasterList, new List<Power>());
+                    MenuView.FinalTotal = 0;
+                    MenuView.SubTotal = 0;
+                    Console.Clear();
+                    vendingMachine.WelcomeAction();
+                }
+                else if (newShopping == "n" || newShopping == "no")
+                {
+                    again = false;
+                    Console.WriteLine("Bye!");
+                }
+                else
+                {
+                    Console.WriteLine("I didn't catch that. Please enter Y or N");
+                }
+            }
         }
     }
 }
--- SVM/RunningTotalView.cs
+++ SVM/RunningTotalView.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SVM
-{
-    class RunningTotalView
-    {
-        public static double VendingMachine { get; set; }
+//namespace SVM
+//{
+//    class RunningTotalView
+//    {
+//        public static double VendingMachine { get; set; }
 
        
 
         
-        public static void PrintSubtotal(List<Power> powers)
-        {
-            for (int i = 0; i < powers.Count; i++)
-            {
-                Console.WriteLine($"Subtotal:Item {powers[i].Name} {powers[i].Price} ");
+//        public static void PrintSubtotal(List<Power> powers)
+//        {
+//            for (int i = 0; i < powers.Count; i++)
+//            {
+//                Console.WriteLine($"Subtotal:Item {powers[i].Name} {powers[i].Price} ");
 
-                Console.WriteLine(MenuView.SubTotal);
+//                Console.WriteLine(MenuView.SubTotal);
                 
-            }
-        }
+//            }
+//        }
 
-    }
+//    }
 
-}
+//}
 
     
 
